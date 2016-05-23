f013a21998af04a014c068b34bbe4ed9f15e45b0
lyawile <webdev271@gmail.com>
2016-05-23 16:28:42 +0300

    initial commit

---
+++ application/views/main.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="container">
+            <div class="container">
+                <div class="jumbotron">
+                    <h1>Welcome to Code Using CI and BT</h1>
+                    <p>Resize this responsive page to see the effect!</p> 
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h3>Column 1</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="col-sm-4">
+                            <?php $this->load->view($content) ?>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
+
