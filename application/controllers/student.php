9b9417b67fffb5fc9cd7eac0e8f473d9a3d7fd00
lyawile <=>
2016-11-08 18:03:50 +0300

    changing system to a greater extent

--- application/controllers/student.php
+++ application/controllers/student.php
@@ -19,41 +19,56 @@ class Student extends CI_Controller {
 
     public function ingizaData() {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $this->form_validation->set_rules('firstname', 'First Name', 'required');
-            $this->form_validation->set_rules('middlename', 'Middle Name', 'required');
-            $this->form_validation->set_rules('surname', 'Last Name', 'required');
-            $this->form_validation->set_rules('gender', 'Gender', 'required');
-            $this->form_validation->set_rules('date', 'Date', 'required');
-            $this->form_validation->set_rules('address', 'Address', 'required');
-            $this->form_validation->set_rules('phonenumber', 'Phone Number', 'required');
-            $this->form_validation->set_rules('dateOfBirth', 'Date of Birth', 'required');
-            $this->form_validation->set_rules('vision', 'Vision', 'required');
+//            $this->form_validation->set_rules('firstname', 'First Name', 'required');
+//            $this->form_validation->set_rules('middlename', 'Middle Name', 'required');
+//            $this->form_validation->set_rules('surname', 'Last Name', 'required');
+//            $this->form_validation->set_rules('gender', 'Gender', 'required');
+//            $this->form_validation->set_rules('date', 'Date', 'required');
+//            $this->form_validation->set_rules('address', 'Address', 'required');
+//            $this->form_validation->set_rules('phonenumber', 'Phone Number', 'required');
+//            $this->form_validation->set_rules('dateOfBirth', 'Date of Birth', 'required');
+//            $this->form_validation->set_rules('vision', 'Vision', 'required');
             //$this->form_validation->set_rules('pic', 'Picture URL', 'required');
-            $config['upload_path'] = './uploads/';
+            $config['upload_path'] = './files/';
             $config['allowed_types'] = 'gif|jpg|png';
-            $config['max_size'] = '100';
+            $config['max_size'] = '1024';
             $config['max_width'] = '1024';
             $config['max_height'] = '768';
-            $config['overwrite'] = 'TRUE';
+            $config['overwrite'] = FALSE;
             $this->load->library('upload', $config);
-            if ($this->form_validation->run() == TRUE && $this->upload->do_upload('pic') == TRUE) {
-                $data = array('upload_data' => $this->upload->data());
-                session_start();
-                $picUrl = $data['upload_data']['file_name'];
-                $_SESSION['picUrl'] = $picUrl;
-                $this->load->model('student_modal');
-                $this->student_modal->insertData();
-                session_commit();
-            }
-             else {
-                $data['successMessage'] = '';
-                $data['error'] = "No file selected";
-                $data['content'] = 'student/register';
-                $this->load->view('main', $data);
+            if ($this->upload->do_upload('pic')) {
+                echo "successfull uploaded";
+//                $data = $this->upload->data();
+//                session_start();
+//                $picUrl = $data['upload_data']['file_name'];
+//                $_SESSION['picUrl'] = $picUrl;
+//                $this->load->model('student_modal');
+//                $this->student_modal->insertData();
+//                session_commit();
+            } else {
+                echo "Nothing works like a charm";
+                echo $this->upload->display_errors();
             }
         }
     }
-    public function load(){
-       
+
+    public function load() {
+        $data['content'] = 'student/load';
+        $this->load->view('main', $data);
     }
+
+    public function loadData() {
+        $config['upload_path'] = './files/';
+        $config['allowed_types'] = 'txt';
+        $config['max_size'] = 100;
+        $this->load->library('upload', $config);
+        if (!$this->upload->do_upload() == false) {
+            $data['mpunga'] = $this->upload->data();
+            $this->load->view('student/data', $data);
+        } else {
+            $data['mpunga'] = $this->upload->display_errors();
+            $this->load->view('student/data', $data);
+        }
+    }
+
 }
---
+++ application/views/student/data.php
@@ -0,0 +1,8 @@
+<?php
+//print_r($mpunga);
+$filename = $mpunga['file_name'];
+$filepath = $mpunga['file_path'];
+
+$fullpath = $filepath.$filename;
+?>
+<a href="http://localhost/smis/files/<?php echo $filename ?>">open a file</a>
